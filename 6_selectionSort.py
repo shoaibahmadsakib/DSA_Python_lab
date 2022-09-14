8b50aac202e26f4f3c430b9da3d53157f7c7bf4c
shoaibahmadsakib <shoaibahmadsakib@gmail.com>
2022-09-14 08:14:30 +0600

    Ada

--- 6_selectionSort.py
+++ 6_selectionSort.py
@@ -1,8 +1,9 @@
 def selectionSort(nums):
-    for i in range(5):
+    size =len(nums)
+    for i in range(size):
         minpos = i
 
-        for j in range(i, 6):
+        for j in range(i,size ):
             if nums[j] < nums[minpos]:
                 minpos = j
 
@@ -11,6 +12,6 @@ def selectionSort(nums):
         nums[i] = nums[minpos]
         nums[minpos] = temp
 
-nums = list(map(int, input("enter array list").split()))
+nums = list(map(int , input("enter: ").split()))
 selectionSort(nums)
 print(nums)
